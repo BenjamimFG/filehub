ca67f7b4fe0740283a40ae42c92061e827f1e0a6
Benjamim Freitas <Ben.FreitasG@gmail.com>
2025-07-02 12:23:14 -0300

    Inicia terraform

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+  instance_class = "db.t3.micro"
+  engine               = "postgres"
+  # TODO: Atualizar usuario e senha do db
+  username             = "root"
+  password             = "root1234"
+  publicly_accessible = true
+  allocated_storage = 10
+  skip_final_snapshot = true
+
+  tags = ["filehub"]
+}
+
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "learn-terraform"
+#   }
+# }
+
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+#   }
+
+#   owners = ["099720109477"] # Canonical
+# }
